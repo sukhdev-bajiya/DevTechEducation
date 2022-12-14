61bcfb2ffeafad80d2406b3afc1d97b5d6f8a5c2
sukhdev-bajiya <bajiya4442@gmail.com>
2022-12-14 09:47:27 +0530

    Client Done

---
+++ src/Admin/Pages/Notifications/Notifications.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Notifications.css";
+import WebsiteUnderConstruction from "../../../ErrorPage/WebsiteUnderConstruction";
+
+function Notifications() {
+  document.title = "Dev Tech Education || Admin || Management";
+  return <WebsiteUnderConstruction />;
+}
+
+export default Notifications;
---
+++ src/Student/Pages/Notifications/Notifications.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Notifications.css";
+import WebsiteUnderConstruction from "../../../ErrorPage/WebsiteUnderConstruction";
+
+function Notifications() {
+  document.title = "Dev Tech Education || Student || Management";
+  return <WebsiteUnderConstruction />;
+}
+
+export default Notifications;
